2a9b512027fc096380437139052d6540199c33e0
Janika2201 <zajcevsmajlik@gmail.com>
2021-02-24 19:36:31 +0200

    информация

--- cinema/Saalid.cs
+++ cinema/Saalid.cs
@@ -24,8 +24,15 @@ namespace cinema
         private PictureBox drake = new PictureBox();
         private PictureBox bob = new PictureBox();
         private static Image Image;
-        private Label label = new Label();
+        private LinkLabel label3 = new LinkLabel();
+        private LinkLabel label = new LinkLabel();
+        private LinkLabel label2 = new LinkLabel();
+        /*private Label label3 = new Label();*/
+        private LinkLabel label4 = new LinkLabel();
+        private LinkLabel label5 = new LinkLabel();
+        private LinkLabel label6 = new LinkLabel();
         
+
 
         public Saalid()
         {
@@ -33,12 +40,43 @@ namespace cinema
             BackgroundImageLayout = ImageLayout.Stretch;
             BackgroundImage = Image.FromFile("../../Image/karl1.jpg");
 
-            /*
-            label.Location = new System.Drawing.Point(80, 10);
-            label.Size = new Size(50, 50);
-            label.Text = "Жанр: Трейлер";
+
+            label.Location = new System.Drawing.Point(145, 9);
+            label.Size = new Size(48, 13);
+            label.Text = "Джокер";
             this.Controls.Add(label);
-            */
+            label.LinkClicked += Label_LinkClicked;
+
+            label2.Location = new System.Drawing.Point(467, 9);
+            label2.Size = new Size(48, 13);
+            label2.Text = "Приво";
+            this.Controls.Add(label2);
+            label2.LinkClicked += Label2_LinkClicked;
+
+            label3.Location = new System.Drawing.Point(777, 9);
+            label3.Size = new Size(48, 13);
+            label3.Text = "Побег";
+            this.Controls.Add(label3);
+            label3.LinkClicked += Label3_LinkClicked;
+            
+
+            label4.Location = new System.Drawing.Point(144, 381);
+            label4.Size = new Size(48, 13);
+            label4.Text = "Дрейк";
+            this.Controls.Add(label4);
+            label4.LinkClicked += Label4_LinkClicked;
+
+            label5.Location = new System.Drawing.Point(472, 383);
+            label5.Size = new Size(48, 13);
+            label5.Text = "Айкарли";
+            this.Controls.Add(label5);
+            label5.LinkClicked += Label5_LinkClicked;
+
+            label6.Location = new System.Drawing.Point(788, 374);
+            label6.Size = new Size(48, 13);
+            label6.Text = "Губка";
+            this.Controls.Add(label6);
+            label6.LinkClicked += Label6_LinkClicked;
 
             pi.BackColor = Color.Gray;
             pi.Location = new System.Drawing.Point(90, 25);
@@ -82,6 +120,36 @@ namespace cinema
             bob.Size = new System.Drawing.Size(210, 330);
             this.Controls.Add(bob);
             bob.Click += Bob_Click;
+        }
+
+        private void Label6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kinopoisk.ru/film/944043/"); 
+        }
+
+        private void Label5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kinopoisk.ru/series/402528/");
+        }
+
+        private void Label4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kinopoisk.ru/film/371100/");
+        }
+
+        private void Label2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kinopoisk.ru/series/1179113/");
+        }
+
+        private void Label_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kinopoisk.ru/film/1048334/");
+        }
+
+        private void Label3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kino-teatr.ru/kino/movie/hollywood/series/22637/annot/");
         }
 
         private void Bob_Click(object sender, EventArgs e)//берем фильм из базы данных и он открывает нам другую форму 
